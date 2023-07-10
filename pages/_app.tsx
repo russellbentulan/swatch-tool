d4b1904217fee7a80c9b9df8a76d7c93c8a94183
Russell Bentulan <russellbentulan@gmail.com>
2023-07-09 23:32:29 -0400

    inital commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  MantineProvider,
+} from "@mantine/core";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps ) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Swatch tool</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
